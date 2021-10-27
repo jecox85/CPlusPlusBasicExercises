dc665ce5ba69ef4c57118aca068fca2d644f548f
James <13830384+jecox85@users.noreply.github.com>
2021-10-26 22:59:14 -0600

    Created program and completed exercise 1.

---
+++ CPlusPlusBasicExercises/CPlusPlusBasicExercises.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    cout << "This program is to do the practice solutions found on www.w3resource.com/cpp-exercises/basic/index.php" << endl << endl;
+    //1. Write a program in C++ to print a welcome text in a separate line. 
+    cout << "Exercise 1. Write a program in C++ to print a welcome text in a separate line.\n";
+    cout << "Welcome!\n\n";
+    return 0;
+}
